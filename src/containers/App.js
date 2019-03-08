f4d690723c4710b1ecd7a534515400fc67daa0ee
Haowei Ye <yehaowei666@163.com>
2019-03-09 00:56:35 +0800

    init App

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+// import { createStore } from 'redux'
+// import { Provider } from 'react-redux'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div>
+        Redux Provider
+      </div>
+    )
+  }
+}
