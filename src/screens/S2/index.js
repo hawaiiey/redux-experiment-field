09764a54b4062682707f79baf77e45c6d9b32d56
Haowei Ye <yehaowei666@163.com>
2019-04-30 16:21:07 +0800

    mobx

--- src/screens/S2/index.js
+++ src/screens/S2/index.js
@@ -1,9 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { observer } from 'mobx-react'
+import { observable } from 'mobx'
 
+@observer
 class S2 extends Component {
+  @observable title = 'Welcome to S2!!!'
+  @observable list = [
+    { value: 'one' },
+    { value: 'two' },
+    { value: 'three' },
+  ]
+
   render () {
     return (
-      <span>Welcome to S2!!!</span>
+      <Fragment>
+        <span>{this.title}</span>
+        <ul>
+          {this.list.map((item, index) => <li key={index}>{item.value}</li>)}
+        </ul>
+        <button onClick={() => this.title = 'Welcome to what???'}>change title</button>
+        <button onClick={() => this.list.push({ value: 'four' })}>add list</button>
+      </Fragment>
     );
   }
 }
