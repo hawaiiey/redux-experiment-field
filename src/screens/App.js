4111fb22f7bed8d1812582d29c53da90883c387a
Haowei Ye <yehaowei666@163.com>
2019-07-10 15:24:12 +0800

    context init

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,33 +1,40 @@
 import React, { Component } from 'react'
-import { Router, Route, browserHistory } from 'react-router'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
+// import { Router, Route, browserHistory } from 'react-router'
+// import { createStore, applyMiddleware } from 'redux'
+// import { Provider } from 'react-redux'
 // import thunkMiddleware from 'redux-thunk'
 // import promiseMiddleware from 'redux-promise'
-import createSagaMiddleware from 'redux-saga'
-import reducer from './reducer'
-import sagas from './S1/sagas'
-import S1 from './S1'
-import S2 from './S2'
+// import createSagaMiddleware from 'redux-saga'
+// import reducer from './reducer'
+// import sagas from './S1/sagas'
+// import S1 from './S1'
+// import S2 from './S2'
 
-const sagaMiddleware = createSagaMiddleware()
-const store = createStore(
-  reducer,
-  applyMiddleware(sagaMiddleware),
-)
-sagaMiddleware.run(sagas)
+// const sagaMiddleware = createSagaMiddleware()
+// const store = createStore(
+//   reducer,
+//   applyMiddleware(sagaMiddleware),
+// )
+// sagaMiddleware.run(sagas)
 
 export default class App extends Component {
+  // render () {
+  //   return (
+  //     <Provider store={store}>
+  //       <Router history={browserHistory}>
+  //         <Route path="/s1" component={S1} />
+  //         <Route path="/s2" component={S2} />
+  //       </Router>
+  //       <S1 />
+  //       <RequestComp />
+  //     </Provider>
+  //   )
+  // }
   render () {
     return (
-      <Provider store={store}>
-        <Router history={browserHistory}>
-          <Route path="/s1" component={S1} />
-          <Route path="/s2" component={S2} />
-        </Router>
-        {/* <S1 />
-        <RequestComp /> */}
-      </Provider>
+      <div>
+        context
+      </div>
     )
   }
 }
--- src/screens/S1/constants.js
+++
@@ -1,7 +0,0 @@
-export const ADD = 'ADD';
-export const REDUCE = 'REDUCE';
-export const REQUEST_START = 'REQUEST_START'
-export const REQUEST_SUCCESS = 'REQUEST_SUCCESS'
-export const REQUEST_ERROR = 'REQUEST_ERROR'
-// redux-promise
-// export const REQUEST = 'REQUEST'
