0580117f2c37980dba2b9befa510ceabd56f0388
Haowei Ye <yehaowei666@163.com>
2019-07-12 11:29:33 +0800

    useContext hook

--- src/screens/ThemeConsumer1.js
+++ src/screens/ThemeConsumer1.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from './ThemeProvider'
 import Text1 from './Text1'
 
 export default function ThemeConsumer1() {
+  const ctx = useContext(ThemeContext)
+  const textProps = {
+    style: ctx.style1,
+  }
+  console.log('Consumer', 1, 'render!!!', ctx)
 
+  // return (
+  //   <ThemeContext.Consumer>
+  //     {
+  //       (ctx) => {
+  //         console.log('Consumer', 1, 'render!!!', ctx)
+  //         const props = {
+  //           style: ctx.style1,
+  //         }
+
+  //         return <Text1 {...props}>context1</Text1>
+  //       }
+  //     }
+  //   </ThemeContext.Consumer>
+  // )
   return (
-    <ThemeContext.Consumer>
-      {
-        (ctx) => {
-          console.log('Consumer', 1, 'render!!!', ctx)
-          const props = {
-            style: ctx.style1,
-          }
-
-          return <Text1 {...props}>context1</Text1>
-        }
-      }
-    </ThemeContext.Consumer>
+    <Text1 {...textProps}>context1</Text1>
   )
 }
--- src/screens/ThemeConsumer2.js
+++ src/screens/ThemeConsumer2.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { ThemeContext } from './ThemeProvider'
 import Text2 from './Text2'
 
 export default function ThemeConsumer2() {
-  return (
-    <ThemeContext.Consumer>
-      {
-        (ctx) => {
-          console.log('Consumer', 2, 'render!!!', ctx)
+  const ctx = useContext(ThemeContext)
+  const textProps = {
+    style: ctx.style2,
+  }
+  console.log('Consumer', 2, 'render!!!', ctx)
 
-          return <Text2 style={ctx.style2}>context2</Text2>
-        }
-      }
-    </ThemeContext.Consumer>
+  // return (
+  //   <ThemeContext.Consumer>
+  //     {
+  //       (ctx) => {
+  //         console.log('Consumer', 2, 'render!!!', ctx)
+
+  //         return <Text2 style={ctx.style2}>context2</Text2>
+  //       }
+  //     }
+  //   </ThemeContext.Consumer>
+  // )
+  return (
+    <Text2 {...textProps}>context1</Text2>
   )
 }
